ea5664cd65db99ab66dda5f9e22261dc4507b522
UNIT Enterprise Systems <55563130+GLPG35@users.noreply.github.com>
2022-05-30 18:23:12 -0300

    v2.0.1
    - Fixed a bug with the theme switcher.
    - Fixed image sizes for the home page.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const colorScheme = window.matchMedia('(prefers-color-scheme: dark)')
 const toggleSwitch = document.querySelector('.inputToggle')
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null
 
-const isChecked = (e) => {
+const isChecked = () => {
     if (toggleSwitch.checked) {
         document.documentElement.setAttribute('data-theme', 'dark')
         localStorage.setItem('theme', 'dark')
@@ -19,7 +19,10 @@ if (currentTheme) {
         toggleSwitch.checked = true
     }
 } else {
-    if (colorScheme.matches) document.documentElement.setAttribute('data-theme', 'dark')
+    if (colorScheme.matches) {
+        document.documentElement.setAttribute('data-theme', 'dark')
+        toggleSwitch.checked = true
+    }
 }
 
 toggleSwitch.addEventListener('change', isChecked)
